c58a99829122d7d49bd8a8a2359a985cf205556b
Peter Farr <Peter@PrismaPhonic.com>
2020-04-26 16:25:01 -0700

    Created some base re-usable logic for services in a service trait.
    Signed-off-by: Peter Farr <Peter@PrismaPhonic.com>

---
+++ src/service.rs
@@ -0,0 +1,24 @@
+use url::Url;
+use reqwest::RequestBuilder;
+use serde::Serialize;
+
+use crate::{BASE_URL, Client};
+
+pub trait Service<T>
+    where T: Serialize,
+{
+    fn request_builder(&self, method: reqwest::Method, rel_url: String, maybe_body_obj: Option<T>) -> RequestBuilder {
+        let url = Url::parse(&format!("{}{}", BASE_URL, rel_url)).unwrap();
+
+        let req_builder: RequestBuilder = self.client().http_client.request(method, url);
+
+        if let Some(body_obj) = &maybe_body_obj {
+            req_builder
+                .json(body_obj)
+        } else {
+            req_builder
+        }
+    }
+
+    fn client(&self) -> &Client;
+}
